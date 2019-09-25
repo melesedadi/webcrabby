2b2e7f3b148dd83f1c2e9efa03b74fd487910f73
melesedadi <dadimeles@yahoo.com>
2019-09-25 11:55:44 -0400

    First Commit

---
+++ src/main/java/com/example/demo/WebcrabbyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcrabbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcrabbyApplication.class, args);
+    }
+
+}
